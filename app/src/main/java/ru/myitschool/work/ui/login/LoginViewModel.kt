4727255684c62c26d58e4413adb887da62cf8ac5
DmitryUniversall <dmitrii.murashov@gmail.com>
2024-12-08 19:58:22 +0300

    Server request methods for LoginViewModel

--- app/src/main/java/ru/myitschool/work/ui/login/LoginViewModel.kt
+++ app/src/main/java/ru/myitschool/work/ui/login/LoginViewModel.kt
@@ -2,13 +2,14 @@ package ru.myitschool.work.ui.login
 
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.camera.core.ImageProcessor.Response
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import ru.myitschool.work.core.components.employee.EmployeeAuthManager
+import ru.myitschool.work.core.components.employee.models.Employee
+import java.util.Optional
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,6 +21,17 @@ class LoginViewModel @Inject constructor(
 
     fun checkUserAuth(login: String): Boolean {
         return EmployeeAuthManager.checkUserAuth(login)
+    }
+
+    fun getEmployeeInfo(login: String): Employee {
+        val employee: Optional<Employee> = EmployeeAuthManager.getEmployeeInfo(login)
+        if (employee.isEmpty) throw RuntimeException("Employee not found")
+
+        return employee.get()
+    }
+
+    fun openDoor(login: String, code: Long): Boolean {
+        return EmployeeAuthManager.openDoor(login, code)
     }
 
     fun saveUserLogin(login: String, sharedPreferences: SharedPreferences) {
