57f21e09409f54ae1826463d24a14b97d316f40d
DmitryUniversall <dmitrii.murashov@gmail.com>
2024-12-09 22:18:53 +0300

    Employee lastVisit type changed to Date

--- app/src/main/java/ru/myitschool/work/core/JsonUtils.java
+++ app/src/main/java/ru/myitschool/work/core/JsonUtils.java
@@ -24,32 +24,4 @@ public class JsonUtils {
     public static Gson getGson() {
         return _gson;
     }
-
-    @NonNull
-    public static <T> T fromJsonWithNulls(@NonNull JsonObject json, @NonNull Class<T> model, @NonNull Gson gson) {
-        JsonObject sanitizedObject = new JsonObject();
-        for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
-            if (entry.getValue() != null && !entry.getValue().isJsonNull()) {
-                sanitizedObject.add(entry.getKey(), entry.getValue());
-            }
-        }
-
-        return gson.fromJson(sanitizedObject, model);
-    }
-
-    @NonNull
-    public static <T> T fromJsonWithNulls(@NonNull JsonObject json, @NonNull Class<T> model) {
-        return fromJsonWithNulls(json, model, _gson);
-    }
-
-    @NonNull
-    public static <T> T fromJsonWithNulls(@NonNull String string, @NonNull Class<T> model, @NonNull Gson gson) {
-        JsonObject json = JsonParser.parseString(string).getAsJsonObject();
-        return fromJsonWithNulls(json, model, gson);
-    }
-
-    @NonNull
-    public static <T> T fromJsonWithNulls(@NonNull String string, @NonNull Class<T> model) {
-        return fromJsonWithNulls(string, model, _gson);
-    }
 }
