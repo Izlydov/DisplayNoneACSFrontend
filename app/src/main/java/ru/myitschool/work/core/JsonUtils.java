07c2a6ea3b08bad52cbf5dd9018c44c34366b2d9
DmitryUniversall <dmitrii.murashov@gmail.com>
2024-12-10 00:35:31 +0300

    hotfix

--- app/src/main/java/ru/myitschool/work/core/JsonUtils.java
+++ app/src/main/java/ru/myitschool/work/core/JsonUtils.java
@@ -4,19 +4,15 @@ import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import java.util.Map;
 
 public class JsonUtils {
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
     private static final @NonNull Gson _gson = _createGson();
 
     @NonNull
     private static Gson _createGson() {
         GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.setDateFormat(Constants.DATE_FORMAT);
+        gsonBuilder.setDateFormat(DATE_FORMAT);
         return gsonBuilder.create();
     }
 
